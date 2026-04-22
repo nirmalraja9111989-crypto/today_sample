3893f2a047734f6d79a5dcb69253645a14422c69
NIrmal Raja <nirmalraja9111989@gmail.com>
2026-04-22 09:16:29 +0530

    Add files via upload

---
+++ crop_analyzer.py
@@ -0,0 +1,22 @@
+# crop_analyzer.py
+
+def analyze_crop(soil_type, rainfall):
+    soil_type = soil_type.lower()
+
+    if rainfall < 0:
+        raise ValueError("Rainfall cannot be negative")
+
+    if soil_type == "clay" and rainfall > 200:
+        return ["Rice"]
+
+    elif soil_type == "sandy" and rainfall < 100:
+        return ["Millet"]
+
+    elif soil_type == "loamy" and 100 <= rainfall <= 200:
+        return ["Wheat", "Maize"]
+
+    elif soil_type == "black" and 50 <= rainfall <= 150:
+        return ["Cotton"]
+
+    else:
+        return ["No suitable crop found"]
